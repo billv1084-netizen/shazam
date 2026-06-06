6a5188ffc78c8f7396a4ae39c9649bf977157f75
billv1084-netizen <billv1084@gmail.com>
2026-06-06 18:04:33 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'shazam-v1.2';
+const CACHE = 'shazam-v1.3';
 const SHELL = ['./', 'index.html', 'manifest.json', 'icon-180.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
